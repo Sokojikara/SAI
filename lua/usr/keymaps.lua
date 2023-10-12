a54a27a230ee857cee32935ace9d063dabab128a
SOKO <Sokojikara>
2023-10-12 18:05:24 +0500

    Keymaps

---
+++ lua/usr/keymaps.lua
@@ -0,0 +1,42 @@
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+local opts = { noremap = true, silent = true }
+
+-- Shorten function name
+local keymap = vim.keymap.set
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<S-h>", "<C-w>h", opts)     -- Left
+keymap("n", "<S-j>", "<C-w>j", opts)        -- Down
+keymap("n", "<S-k>", "<C-w>k", opts)      -- Up
+keymap("n", "<S-l>", "<C-w>l", opts)        -- Right
+
+
+-- Insert --
+-- Press jk fast to exit insert mode 
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv^", opts)
+keymap("v", ">", ">gv^", opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
