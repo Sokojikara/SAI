d9ffbac754580bd795432d99aa71466bd942ab04
SOKO <Sokojikara>
2023-10-12 18:04:56 +0500

    Options

---
+++ lua/usr/options.lua
@@ -0,0 +1,27 @@
+local vopt = vim.opt
+
+vopt.nu = true
+vopt.relativenumber = true
+
+vopt.tabstop = 4
+vopt.softtabstop = 4
+vopt.shiftwidth = 4
+vopt.expandtab = true
+
+vopt.smartindent = true
+
+vopt.wrap = true
+
+vopt.swapfile = false
+vopt.backup = false
+
+vopt.hlsearch = false
+vopt.incsearch = true
+
+vopt.termguicolors = true
+
+vopt.scrolloff = 8
+vopt.signcolumn = "yes"
+vopt.isfname:append("@-@")
+
+vopt.updatetime = 50
